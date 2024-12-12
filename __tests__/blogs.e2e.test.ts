7de36638846e65e5ef6b426c8680d077c598603a
Vilyam <z.v.m.365@gmail.com>
2024-12-12 23:31:29 +0300

    corrected the text for creating a blog

--- __tests__/blogs.e2e.test.ts
+++ __tests__/blogs.e2e.test.ts
@@ -14,17 +14,17 @@ describe('/blogs', () => {
     it('should create a new blog', async () => {
         const res = await req
             .post(SETTINGS.PATH.BLOGS)
-            .send({
-                name: 'blog1',
-                description: 'description1',
-                websiteUrl: 'https://blog1.com'
-            })
-            .expect(201)
-            .expect(res.body.name).toEqual(blog1.name)
-            .expect(res.body.description).toEqual(blog1.description)
-            .expect(res.body.websiteUrl).toEqual(blog1.websiteUrl)
-            .expect(typeof res.body.id).toEqual('string')
-            .expect(res.body).toEqual(db.blogs[0]);
+            .send(blog1)
+            .expect(201);
+
+        expect(res.body).toEqual({
+            id: expect.any(String),
+            name: blog1.name,
+            description: blog1.description,
+            websiteUrl: blog1.websiteUrl
+        });
+
+        expect(res.body).toEqual(db.blogs[0]);
 
         console_log(res.body, res.status, 'Test 2: post(/blogs)\n');
     });
