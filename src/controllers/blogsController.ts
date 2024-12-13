76c70feff35b26fac1d1e6a55c6df1309567e25d
Vilyam <z.v.m.365@gmail.com>
2024-12-13 21:05:26 +0300

    created the controller logic to get a blog by

--- src/controllers/blogsController.ts
+++ src/controllers/blogsController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import {BlogInputModel, BlogViewModel} from "../types/input-output-types/blogs-types";
 import {blogsRepository} from "../repositoryes/blogs-repository";
+import {RequestWithParams} from "../types/input-output-types/request-types";
 
 const blogsController = {
     getBlogs: (
@@ -13,9 +14,19 @@ const blogsController = {
             .json(blogs);
     },
     getBlog: (
-        req: Request,
+        req: RequestWithParams<{ id: string }>,
         res: Response) => {
+        const foundBlog: BlogViewModel | undefined = blogsRepository.getBlogById(req.params.id);
 
+        if (!foundBlog) {
+            res
+                .status(404)
+                .end();
+        }
+
+        res
+            .status(200)
+            .json(foundBlog)
     },
     createBlog: (
         req: Request<BlogInputModel>,
