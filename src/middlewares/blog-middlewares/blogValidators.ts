1f9dee33dda57e1752505355cf9714dc22e15bb6
Vilyam <z.v.m.365@gmail.com>
2024-12-14 21:18:03 +0300

    created middleware to validate data when creating a blog

---
+++ src/middlewares/blog-middlewares/blogValidators.ts
@@ -0,0 +1,27 @@
+import {body} from "express-validator";
+
+const blogNameInputValidator =
+    body('name')
+        .isString().withMessage('The "name" field must be of the string type.')
+        .trim()
+        .isLength({min: 1, max: 15}).withMessage('The length of the "name" field should be from 1 to 15.');
+
+const blogDescriptionInputValidator =
+    body('description')
+        .isString().withMessage('The "description" field must be of the string type.')
+        .trim()
+        .isLength({min: 1, max: 500}).withMessage('The length of the "description" field should be from 1 to 500.');
+
+const blogWebsiteUrlInputValidator =
+    body('websiteUrl')
+        .isString().withMessage('The "websiteUrl" field must be of the string type.')
+        .trim()
+        .isLength({min: 1, max: 100}).withMessage('The length of the "description" field should be from 1 to 100.')
+        .isURL().withMessage('Invalid URL. The field must start with "https://" and match the pattern:' +
+        ' "https://example.com/path".');
+
+export {
+    blogNameInputValidator,
+    blogDescriptionInputValidator,
+    blogWebsiteUrlInputValidator
+};
--- src/routes/blogsRouter.ts
+++ src/routes/blogsRouter.ts
@@ -1,11 +1,21 @@
 import {Router} from "express";
 import {blogsController} from '../controllers/blogsController';
+import {
+    blogDescriptionInputValidator,
+    blogNameInputValidator,
+    blogWebsiteUrlInputValidator
+} from "../middlewares/blog-middlewares/blogValidators";
 
 const blogsRouter = Router();
 
 blogsRouter.get('/', blogsController.getBlogs);
 blogsRouter.get('/:id', blogsController.getBlog);
-blogsRouter.post('/', blogsController.createBlog);
+blogsRouter.post('/',
+    blogNameInputValidator,
+    blogDescriptionInputValidator,
+    blogWebsiteUrlInputValidator,
+    blogsController.createBlog);
+
 blogsRouter.put('/:id', blogsController.updateBlog);
 blogsRouter.delete('/:id', blogsController.deleteBlog);
 
