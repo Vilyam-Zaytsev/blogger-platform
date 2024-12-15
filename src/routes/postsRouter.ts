961ba174f33fca94d443e4d794196b6b9fd46ed9
Vilyam <z.v.m.365@gmail.com>
2024-12-15 13:01:38 +0300

    added validators to postsRouter

--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -1,6 +1,12 @@
 import {Router} from "express";
 import {postsController} from '../controllers/postsController';
 import {authMiddleware} from "../middlewares/global-middlewares/authorization-middleware";
+import {
+    postContentInputValidator,
+    postShortDescriptionInputValidator,
+    postTitleInputValidator
+} from "../middlewares/post-middlewares/postValidators";
+import {inputCheckErrorsMiddleware} from "../middlewares/global-middlewares/input-check-errors-middleware";
 
 const postsRouter = Router();
 
@@ -8,6 +14,10 @@ postsRouter.get('/', postsController.getPosts);
 postsRouter.get('/:id', postsController.getPost);
 postsRouter.post('/',
     authMiddleware,
+    postTitleInputValidator,
+    postShortDescriptionInputValidator,
+    postContentInputValidator,
+    inputCheckErrorsMiddleware,
     postsController.createPost
 );
 postsRouter.put('/:id', postsController.updatePost);
