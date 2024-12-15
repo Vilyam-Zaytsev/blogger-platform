9ebaac0f0cc20654c06e0f423590014c67738dd8
Vilyam <z.v.m.365@gmail.com>
2024-12-15 14:55:51 +0300

    add testController

--- src/controllers/testsController.ts
+++ src/controllers/testsController.ts
@@ -1,11 +1,17 @@
 import {Request, Response} from "express";
+import {db} from "../db/db";
 
 const testsController = {
     deleteAllData: (
         req: Request,
         res: Response) => {
+            db.blogs = [];
+            db.posts = [];
 
-    },
+            res
+                .status(204)
+                .json({'message': 'All data has been deleted.'});
+        }
 };
 
 export {testsController};
