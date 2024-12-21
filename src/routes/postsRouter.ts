53be7476e813300f992f48f3029be94a5cc0db98
Vilyam <z.v.m.365@gmail.com>
2024-12-21 10:48:46 +0300

    fix

--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -1,39 +1,39 @@
-// import {Router} from "express";
-// import {postsController} from '../controllers/postsController';
-// import {authMiddleware} from "../middlewares/global-middlewares/authorization-middleware";
-// import {
-//     postBlogIdInputValidator,
-//     postContentInputValidator,
-//     postShortDescriptionInputValidator,
-//     postTitleInputValidator
-// } from "../middlewares/post-middlewares/postValidators";
-// import {inputCheckErrorsMiddleware} from "../middlewares/global-middlewares/input-check-errors-middleware";
-//
-// const postsRouter = Router();
-//
-// postsRouter.get('/', postsController.getPosts);
-// postsRouter.get('/:id', postsController.getPost);
-// postsRouter.post('/',
-//     authMiddleware,
-//     postTitleInputValidator,
-//     postShortDescriptionInputValidator,
-//     postContentInputValidator,
-//     postBlogIdInputValidator,
-//     inputCheckErrorsMiddleware,
-//     postsController.createPost
-// );
-// postsRouter.put('/:id',
-//     authMiddleware,
-//     postTitleInputValidator,
-//     postShortDescriptionInputValidator,
-//     postContentInputValidator,
-//     postBlogIdInputValidator,
-//     inputCheckErrorsMiddleware,
-//     postsController.updatePost
-// );
-// postsRouter.delete('/:id',
-//     authMiddleware,
-//     postsController.deletePost
-// );
-//
-// export {postsRouter};
+import {Router} from "express";
+import {postsController} from '../controllers/postsController';
+import {authMiddleware} from "../middlewares/global-middlewares/authorization-middleware";
+import {
+    postBlogIdInputValidator,
+    postContentInputValidator,
+    postShortDescriptionInputValidator,
+    postTitleInputValidator
+} from "../middlewares/post-middlewares/postValidators";
+import {inputCheckErrorsMiddleware} from "../middlewares/global-middlewares/input-check-errors-middleware";
+
+const postsRouter = Router();
+
+postsRouter.get('/', postsController.getPosts);
+postsRouter.get('/:id', postsController.getPost);
+postsRouter.post('/',
+    authMiddleware,
+    postTitleInputValidator,
+    postShortDescriptionInputValidator,
+    postContentInputValidator,
+    postBlogIdInputValidator,
+    inputCheckErrorsMiddleware,
+    postsController.createPost
+);
+postsRouter.put('/:id',
+    authMiddleware,
+    postTitleInputValidator,
+    postShortDescriptionInputValidator,
+    postContentInputValidator,
+    postBlogIdInputValidator,
+    inputCheckErrorsMiddleware,
+    postsController.updatePost
+);
+postsRouter.delete('/:id',
+    authMiddleware,
+    postsController.deletePost
+);
+
+export {postsRouter};
