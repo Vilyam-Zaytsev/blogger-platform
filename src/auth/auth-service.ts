71998a79da7959f5794b3352120f0bb5a32585a8
Vilyam <z.v.m.365@gmail.com>
2025-01-12 13:01:24 +0300

    changed the refund from checkUserCredentials

--- src/auth/auth-service.ts
+++ src/auth/auth-service.ts
@@ -2,12 +2,14 @@ import {bcryptService} from "../common/services/bcryptService";
 import {LoginInputType} from "../common/types/input-output-types/login-types";
 import {UserDbType} from "../users/types/user-db-type";
 import {usersRepository} from "../users/repositoryes/users-repository";
+import {ResultStatusType} from "../common/types/result-types/result-status-type";
+import {ResultType} from "../common/types/result-types/result-type";
 
 const authService = {
     async login(authParamsDto: LoginInputType): Promise<boolean> {
-        return this.checkUserCredentials(authParamsDto);
+        const result: ResultType = await this.checkUserCredentials(authParamsDto);
     },
-    async checkUserCredentials(authParamsDto: LoginInputType): Promise<boolean> {
+    async checkUserCredentials(authParamsDto: LoginInputType): Promise<ResultType> {
         const {
             loginOrEmail,
             password
@@ -16,14 +18,37 @@ const authService = {
         const isUser: UserDbType | null = await usersRepository
             .findByLoginOrEmail(loginOrEmail);
 
-        if (!isUser) return false;
+        if (!isUser) {
+            return {
+                status: ResultStatusType.NotFound,
+                errorMessage: 'loginOrEmail incorrect',
+                extensions: [{
+                    field: 'loginOrEmail',
+                    message: 'There is no user with such data.',
+                }],
+                data: null
+            } as ResultType
+        }
 
         const isPasswordCorrect = await bcryptService
             .checkPassword(password, isUser.passwordHash);
 
-        if (!isPasswordCorrect) return false;
+        if (!isPasswordCorrect) {
+           return  {
+                status: ResultStatusType.BadRequest,
+                    errorMessage: 'password incorrect',
+                extensions: [{
+                field: 'password',
+                message: 'Invalid password.',
+            }],
+                data: null
+            } as ResultType
+        }
 
-        return true;
+        return {
+            status: ResultStatusType.Success,
+            data: isUser
+        } as ResultType
     }
 };
 
