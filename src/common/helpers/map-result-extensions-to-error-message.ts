7a9ae8c28754b5d81bc1d8a8d0de333894b48065
Vilyam <z.v.m.365@gmail.com>
2025-01-14 16:40:50 +0300

    created tests /auth

--- src/common/helpers/map-result-extensions-to-error-message.ts
+++ src/common/helpers/map-result-extensions-to-error-message.ts
@@ -3,7 +3,7 @@ import {OutputErrorsType} from "../types/input-output-types/output-errors-type";
 
 const mapResultExtensionsToErrorMessage = (extensions: ExtensionsType): OutputErrorsType => {
     return {
-        errorsMessage: [...extensions]
+        errorsMessages: [...extensions]
     };
 };
 
