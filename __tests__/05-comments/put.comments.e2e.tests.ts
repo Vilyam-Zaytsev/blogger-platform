1977cc70bee24b0107bb9393aa3e1bba1ea5b561
Vilyam <z.v.m.365@gmail.com>
2025-01-20 23:09:06 +0300

    Implemented comment updating functionality and developed tests for updating comments.

--- __tests__/05-comments/put.comments.e2e.tests.ts
+++ __tests__/05-comments/put.comments.e2e.tests.ts
@@ -11,7 +11,7 @@ import {UserDbType} from "../../src/02-users/types/user-db-type";
 import {BlogDbType} from "../../src/03-blogs/types/blog-db-type";
 import {PostDbType} from "../../src/04-posts/types/post-db-type";
 import {CommentDbType} from "../../src/05-comments/types/comment-db-type";
-import {clearPresets, commentPropertyMap, comments, incorrectAccessToken, presets} from "../helpers/datasets-for-tests";
+import {clearPresets, comments, incorrectAccessToken, presets} from "../helpers/datasets-for-tests";
 import {blogsTestManager} from "../helpers/blogs-test-manager";
 import {postsTestManager} from "../helpers/posts-test-manager";
 import {usersTestManager} from "../helpers/users-test-manager";
@@ -21,10 +21,7 @@ import {console_log, generateRandomString, req} from "../helpers/test-helpers";
 import {SETTINGS} from "../../src/common/settings";
 import {CommentViewModel} from "../../src/05-comments/types/input-output-types";
 import {OutputErrorsType} from "../../src/common/types/input-output-types/output-errors-type";
-import {PaginationResponse, SortDirection} from "../../src/common/types/input-output-types/pagination-sort-types";
-import {commentsService} from "../../src/05-comments/comments-service";
 import {commentsTestManager} from "../helpers/comments-test-manager";
-import {createPaginationAndSortFilter} from "../../src/common/helpers/create-pagination-and-sort-filter";
 
 let mongoServer: MongoMemoryServer;
 let client: MongoClient;
@@ -103,47 +100,7 @@ describe('PUT /comments', () => {
 
         console_log(resPutComments.body, resPutComments.status, 'Test 1: put(/comments)');
     });
-    it('should return an array with three comments.', async () => {
-
-        await blogsTestManager
-            .createBlog(1);
-
-        await postsTestManager
-            .createPost(1);
-
-        await usersTestManager
-            .createUser(1);
-
-        await authTestManager
-            .login(presets.users.map(u => u.login));
-
-        await commentsTestManager
-            .createComments(3);
-
-        const resGetComments: Response = await req
-            .get(`${SETTINGS.PATH.POSTS}/${presets.posts[0].id}${SETTINGS.PATH.COMMENTS}`)
-            .expect(SETTINGS.HTTP_STATUSES.OK_200);
-
-        for (let i = 0; i < resGetComments.body.items.length; i++) {
-            expect(resGetComments.body.items[i]).toEqual(
-                commentsTestManager.filterAndSort(
-                    presets.comments,
-                    createPaginationAndSortFilter({
-                        pageNumber: '1',
-                        pageSize: '10',
-                        sortBy: 'createdAt',
-                        sortDirection: SortDirection.Descending
-                    }),
-                    commentPropertyMap
-                )[i]
-            );
-        }
-
-        expect(resGetComments.body.items.length).toEqual(3);
-
-        console_log(resGetComments.body, resGetComments.status, 'Test 3: get(/comments)');
-    });
-    it('should return comment found by id.', async () => {
+    it('should not update the comment if the user is not logged in.', async () => {
 
         await blogsTestManager
             .createBlog(1);
@@ -160,15 +117,26 @@ describe('PUT /comments', () => {
         await commentsTestManager
             .createComments(1);
 
-        const resGetComment = await req
+        const resPutComments: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .set(
+                'Authorization',
+                incorrectAccessToken
+            )
+            .send({
+                content: comments[1]
+            })
+            .expect(SETTINGS.HTTP_STATUSES.UNAUTHORIZED_401);
+
+        const resGetComment: Response = await req
             .get(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
             .expect(SETTINGS.HTTP_STATUSES.OK_200);
 
-        expect(resGetComment.body).toEqual(presets.comments[0])
+        expect(resGetComment.body).toEqual<CommentViewModel>(presets.comments[0])
 
-        console_log(resGetComment.body, resGetComment.status, 'Test 4: getById(/comments)');
+        console_log(resPutComments.body, resPutComments.status, 'Test 2: put(/comments)');
     });
-    it('should return the 404 not found error (if the comment with this ID does not exist).', async () => {
+    it('should not update the comment if the data in the request body is incorrect (an empty object is passed).', async () => {
 
         await blogsTestManager
             .createBlog(1);
@@ -185,16 +153,242 @@ describe('PUT /comments', () => {
         await commentsTestManager
             .createComments(1);
 
-        const resGetComment_1: Response = await req
-            .get(`${SETTINGS.PATH.COMMENTS}/${new ObjectId()}`)
+        const resPutComments: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .set(
+                'Authorization',
+                `Bearer ${presets.accessTokens[0].accessToken}`
+            )
+            .send({})
+            .expect(SETTINGS.HTTP_STATUSES.BAD_REQUEST_400);
+
+        expect(resPutComments.body).toEqual<OutputErrorsType>({
+            errorsMessages: [
+                {
+                    field: 'content',
+                    message: 'The "content" field must be of the string type.'
+                }
+            ]
+        });
+
+        const resGetComment: Response = await req
+            .get(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+
+        expect(resGetComment.body).toEqual<CommentViewModel>(presets.comments[0])
+
+        console_log(resPutComments.body, resPutComments.status, 'Test 3: put(/comments)');
+    });
+    it('should not update the comment if the data in the request body is incorrect (the content field contains data of the number type).', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(1);
+
+        await usersTestManager
+            .createUser(1);
+
+        await authTestManager
+            .login(presets.users.map(u => u.login));
+
+        await commentsTestManager
+            .createComments(1);
+
+        const resPutComments: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .set(
+                'Authorization',
+                `Bearer ${presets.accessTokens[0].accessToken}`
+            )
+            .send({
+                content: 123
+            })
+            .expect(SETTINGS.HTTP_STATUSES.BAD_REQUEST_400);
+
+        expect(resPutComments.body).toEqual<OutputErrorsType>({
+            errorsMessages: [
+                {
+                    field: 'content',
+                    message: 'The "content" field must be of the string type.'
+                }
+            ]
+        });
+
+        const resGetComment: Response = await req
+            .get(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+
+        expect(resGetComment.body).toEqual<CommentViewModel>(presets.comments[0])
+
+        console_log(resPutComments.body, resPutComments.status, 'Test 4: put(/comments)');
+    });
+    it('should not update the comment if the data in the request body is incorrect (the content field is less than 20 characters long).', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(1);
+
+        await usersTestManager
+            .createUser(1);
+
+        await authTestManager
+            .login(presets.users.map(u => u.login));
+
+        await commentsTestManager
+            .createComments(1);
+
+        const resPutComments: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .set(
+                'Authorization',
+                `Bearer ${presets.accessTokens[0].accessToken}`
+            )
+            .send({
+                content: generateRandomString(19)
+            })
+            .expect(SETTINGS.HTTP_STATUSES.BAD_REQUEST_400);
+
+        expect(resPutComments.body).toEqual<OutputErrorsType>({
+            errorsMessages: [
+                {
+                    field: 'content',
+                    message: 'The length of the "content" field should be from 20 to 300.'
+                }
+            ]
+        });
+
+        const resGetComment: Response = await req
+            .get(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+
+        expect(resGetComment.body).toEqual<CommentViewModel>(presets.comments[0])
+
+        console_log(resPutComments.body, resPutComments.status, 'Test 5: put(/comments)');
+    });
+    it('should not update the comment if the data in the request body is incorrect (the content field is more than 300 characters long).', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(1);
+
+        await usersTestManager
+            .createUser(1);
+
+        await authTestManager
+            .login(presets.users.map(u => u.login));
+
+        await commentsTestManager
+            .createComments(1);
+
+        const resPutComments: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .set(
+                'Authorization',
+                `Bearer ${presets.accessTokens[0].accessToken}`
+            )
+            .send({
+                content: generateRandomString(301)
+            })
+            .expect(SETTINGS.HTTP_STATUSES.BAD_REQUEST_400);
+
+        expect(resPutComments.body).toEqual<OutputErrorsType>({
+            errorsMessages: [
+                {
+                    field: 'content',
+                    message: 'The length of the "content" field should be from 20 to 300.'
+                }
+            ]
+        });
+
+        const resGetComment: Response = await req
+            .get(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+
+        expect(resGetComment.body).toEqual<CommentViewModel>(presets.comments[0])
+
+        console_log(resPutComments.body, resPutComments.status, 'Test 6: put(/comments)');
+    });
+    it('should not update comments if the user in question is not the owner of the comment.', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(1);
+
+        await usersTestManager
+            .createUser(2);
+
+        await authTestManager
+            .login(presets.users.map(u => u.login));
+
+        await commentsTestManager
+            .createComments(1);
+
+        const resPutComments_1: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .set(
+                'Authorization',
+                `Bearer ${presets.accessTokens[1].accessToken}`
+            )
+            .send({
+                content: comments[1]
+            })
+            .expect(SETTINGS.HTTP_STATUSES.FORBIDDEN_403);
+
+        const resPutComments_2: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
+            .set(
+                'Authorization',
+                `Bearer ${presets.accessTokens[0].accessToken}`
+            )
+            .send({
+                content: comments[1]
+            })
+            .expect(SETTINGS.HTTP_STATUSES.NO_CONTENT_204);
+
+        console_log(resPutComments_1.body, resPutComments_1.status, 'Test 7: put(/comments)');
+    });
+    it('should not update comments if the comment does not exist.', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(1);
+
+        await usersTestManager
+            .createUser(1);
+
+        await authTestManager
+            .login(presets.users.map(u => u.login));
+
+        await commentsTestManager
+            .createComments(1);
+
+        const resPutComments: Response = await req
+            .put(`${SETTINGS.PATH.COMMENTS}/${new ObjectId()}`)
+            .set(
+                'Authorization',
+                `Bearer ${presets.accessTokens[0].accessToken}`
+            )
+            .send({
+                content: comments[1]
+            })
             .expect(SETTINGS.HTTP_STATUSES.NOT_FOUND_404);
 
-        const resGetComment_2: Response = await req
+        const resGetComment: Response = await req
             .get(`${SETTINGS.PATH.COMMENTS}/${presets.comments[0].id}`)
-            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+            .expect(SETTINGS.HTTP_STATUSES.OK_200)
 
-        expect(resGetComment_2.body).toEqual(presets.comments[0])
+        expect(resGetComment.body).toEqual<CommentViewModel>(presets.comments[0]);
 
-        console_log(resGetComment_1.body, resGetComment_1.status, 'Test 5: getById(/comments)');
+        console_log(resPutComments.body, resPutComments.status, 'Test 8: put(/comments)');
     });
 });
