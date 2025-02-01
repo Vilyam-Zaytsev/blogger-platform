1442049346fb2e5f43bba0dd3e866dda273aa70d
Vilyam <z.v.m.365@gmail.com>
2025-02-01 18:30:35 +0300

    fix import

--- __tests__/04-posts/sort.pagination.posts.e2e.test.ts
+++ __tests__/04-posts/sort.pagination.posts.e2e.test.ts
@@ -1,10 +1,19 @@
-import {console_log, encodingAdminDataInBase64, generateRandomString, req} from '../helpers/test-helpers';
+import {console_log, req} from '../helpers/test-helpers';
 import {SETTINGS} from "../../src/common/settings";
-import {blog, clearPresets, post, postPropertyMap, presets} from "../helpers/datasets-for-tests";
+import {
+    clearPresets,
+    postPropertyMap,
+    presets
+} from "../helpers/datasets-for-tests";
 import {blogsTestManager} from "../helpers/managers/03_blogs-test-manager";
 import {MongoMemoryServer} from "mongodb-memory-server";
-import {MongoClient, ObjectId} from "mongodb";
-import {blogsCollection, postsCollection, setBlogsCollection, setPostsCollection} from "../../src/db/mongoDb";
+import {MongoClient} from "mongodb";
+import {
+    blogsCollection,
+    postsCollection,
+    setBlogsCollection,
+    setPostsCollection
+} from "../../src/db/mongoDb";
 import {BlogDbType} from "../../src/03-blogs/types/blog-db-type";
 import {PostDbType} from "../../src/04-posts/types/post-db-type";
 import {postsTestManager} from "../helpers/managers/04_posts-test-manager";
@@ -39,161 +48,161 @@ beforeEach(async () => {
     clearPresets();
 });
 
-    describe('pagination, sort /posts', () => {
+describe('pagination, sort /posts', () => {
 
-        it('should use default pagination values when none are provided by the client.', async () => {
+    it('should use default pagination values when none are provided by the client.', async () => {
 
-            await blogsTestManager
-                .createBlog(1);
+        await blogsTestManager
+            .createBlog(1);
 
-            await postsTestManager
-                .createPost(11);
+        await postsTestManager
+            .createPost(11);
 
-            const resGetPosts: Response = await req
-                .get(SETTINGS.PATH.POSTS)
-                .expect(SETTINGS.HTTP_STATUSES.OK_200);
+        const resGetPosts: Response = await req
+            .get(SETTINGS.PATH.POSTS)
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
 
-            expect(resGetPosts.body).toEqual({
-                pagesCount: 2,
-                page: 1,
-                pageSize: 10,
-                totalCount: 11,
-                items: postsTestManager.filterAndSort(
-                    presets.posts,
-                    createPaginationAndSortFilter({
-                        pageNumber: '1',
-                        pageSize: '10',
-                        sortBy: 'createdAt',
-                        sortDirection: SortDirection.Descending,
-                    }),
-                    postPropertyMap
-                )
-            });
-
-            expect(resGetPosts.body.items.length).toEqual(10);
-
-            console_log(resGetPosts.body, resGetPosts.status, 'Test 1: pagination, sort(/posts)');
+        expect(resGetPosts.body).toEqual({
+            pagesCount: 2,
+            page: 1,
+            pageSize: 10,
+            totalCount: 11,
+            items: postsTestManager.filterAndSort(
+                presets.posts,
+                createPaginationAndSortFilter({
+                    pageNumber: '1',
+                    pageSize: '10',
+                    sortBy: 'createdAt',
+                    sortDirection: SortDirection.Descending,
+                }),
+                postPropertyMap
+            )
         });
 
-        it('should use client-provided pagination values to return the correct subset of data.', async () => {
+        expect(resGetPosts.body.items.length).toEqual(10);
 
-            await blogsTestManager
-                .createBlog(1);
-
-            await postsTestManager
-                .createPost(11);
-
-            const resGetPosts: Response = await req
-                .get(SETTINGS.PATH.POSTS)
-                .query({
-                    sortBy: 'title',
-                    sortDirection: 'asc',
-                    pageNumber: 2,
-                    pageSize: 3
-                })
-                .expect(SETTINGS.HTTP_STATUSES.OK_200);
-
-            expect(resGetPosts.body).toEqual({
-                pagesCount: 4,
-                page: 2,
-                pageSize: 3,
-                totalCount: 11,
-                items: postsTestManager.filterAndSort(
-                    presets.posts,
-                    createPaginationAndSortFilter({
-                        pageNumber: '2',
-                        pageSize: '3',
-                        sortBy: 'title',
-                        sortDirection: SortDirection.Ascending,
-                    }),
-                    postPropertyMap
-                )
-            });
-
-            expect(resGetPosts.body.items.length).toEqual(3);
-
-            console_log(resGetPosts.body, resGetPosts.status, 'Test 2: pagination, sort(/posts)');
-        });
-
-        it('should use client-provided pagination values to return the correct subset of data (№2).', async () => {
-
-            await blogsTestManager
-                .createBlog(1);
-
-            await postsTestManager
-                .createPost(11);
-
-            const resGetPosts: Response = await req
-                .get(SETTINGS.PATH.POSTS)
-                .query({
-                    sortBy: 'content',
-                    sortDirection: 'desc',
-                    pageNumber: 6,
-                    pageSize: 2
-                })
-                .expect(SETTINGS.HTTP_STATUSES.OK_200);
-
-            expect(resGetPosts.body).toEqual({
-                pagesCount: 6,
-                page: 6,
-                pageSize: 2,
-                totalCount: 11,
-                items: postsTestManager.filterAndSort(
-                    presets.posts,
-                    createPaginationAndSortFilter({
-                        pageNumber: '6',
-                        pageSize: '2',
-                        sortBy: 'content',
-                        sortDirection: SortDirection.Descending,
-                    }),
-                    postPropertyMap
-                )
-            });
-
-            expect(resGetPosts.body.items.length).toEqual(1);
-
-            console_log(resGetPosts.body, resGetPosts.status, 'Test 3: pagination, sort(/posts)');
-        });
-
-        it('should use client-provided pagination values to return the correct subset of data (№3).', async () => {
-
-            await blogsTestManager
-                .createBlog(1);
-
-            await postsTestManager
-                .createPost(11);
-
-            const resGetPosts: Response = await req
-                .get(SETTINGS.PATH.POSTS)
-                .query({
-                    sortBy: 'shortDescription',
-                    sortDirection: 'asc',
-                    pageNumber: 6,
-                    pageSize: 2
-                })
-                .expect(SETTINGS.HTTP_STATUSES.OK_200);
-
-            expect(resGetPosts.body).toEqual({
-                pagesCount: 6,
-                page: 6,
-                pageSize: 2,
-                totalCount: 11,
-                items: postsTestManager.filterAndSort(
-                    presets.posts,
-                    createPaginationAndSortFilter({
-                        pageNumber: '6',
-                        pageSize: '2',
-                        sortBy: 'shortDescription',
-                        sortDirection: SortDirection.Ascending,
-                    }),
-                    postPropertyMap
-                )
-            });
-
-            expect(resGetPosts.body.items.length).toEqual(1);
-
-            expect(resGetPosts.body.items.length).toEqual(1);
-
-            console_log(resGetPosts.body, resGetPosts.status, 'Test 4: pagination, sort(/posts)');
-        });
+        console_log(resGetPosts.body, resGetPosts.status, 'Test 1: pagination, sort(/posts)');
     });
+
+    it('should use client-provided pagination values to return the correct subset of data.', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(11);
+
+        const resGetPosts: Response = await req
+            .get(SETTINGS.PATH.POSTS)
+            .query({
+                sortBy: 'title',
+                sortDirection: 'asc',
+                pageNumber: 2,
+                pageSize: 3
+            })
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+
+        expect(resGetPosts.body).toEqual({
+            pagesCount: 4,
+            page: 2,
+            pageSize: 3,
+            totalCount: 11,
+            items: postsTestManager.filterAndSort(
+                presets.posts,
+                createPaginationAndSortFilter({
+                    pageNumber: '2',
+                    pageSize: '3',
+                    sortBy: 'title',
+                    sortDirection: SortDirection.Ascending,
+                }),
+                postPropertyMap
+            )
+        });
+
+        expect(resGetPosts.body.items.length).toEqual(3);
+
+        console_log(resGetPosts.body, resGetPosts.status, 'Test 2: pagination, sort(/posts)');
+    });
+
+    it('should use client-provided pagination values to return the correct subset of data (№2).', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(11);
+
+        const resGetPosts: Response = await req
+            .get(SETTINGS.PATH.POSTS)
+            .query({
+                sortBy: 'content',
+                sortDirection: 'desc',
+                pageNumber: 6,
+                pageSize: 2
+            })
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+
+        expect(resGetPosts.body).toEqual({
+            pagesCount: 6,
+            page: 6,
+            pageSize: 2,
+            totalCount: 11,
+            items: postsTestManager.filterAndSort(
+                presets.posts,
+                createPaginationAndSortFilter({
+                    pageNumber: '6',
+                    pageSize: '2',
+                    sortBy: 'content',
+                    sortDirection: SortDirection.Descending,
+                }),
+                postPropertyMap
+            )
+        });
+
+        expect(resGetPosts.body.items.length).toEqual(1);
+
+        console_log(resGetPosts.body, resGetPosts.status, 'Test 3: pagination, sort(/posts)');
+    });
+
+    it('should use client-provided pagination values to return the correct subset of data (№3).', async () => {
+
+        await blogsTestManager
+            .createBlog(1);
+
+        await postsTestManager
+            .createPost(11);
+
+        const resGetPosts: Response = await req
+            .get(SETTINGS.PATH.POSTS)
+            .query({
+                sortBy: 'shortDescription',
+                sortDirection: 'asc',
+                pageNumber: 6,
+                pageSize: 2
+            })
+            .expect(SETTINGS.HTTP_STATUSES.OK_200);
+
+        expect(resGetPosts.body).toEqual({
+            pagesCount: 6,
+            page: 6,
+            pageSize: 2,
+            totalCount: 11,
+            items: postsTestManager.filterAndSort(
+                presets.posts,
+                createPaginationAndSortFilter({
+                    pageNumber: '6',
+                    pageSize: '2',
+                    sortBy: 'shortDescription',
+                    sortDirection: SortDirection.Ascending,
+                }),
+                postPropertyMap
+            )
+        });
+
+        expect(resGetPosts.body.items.length).toEqual(1);
+
+        expect(resGetPosts.body.items.length).toEqual(1);
+
+        console_log(resGetPosts.body, resGetPosts.status, 'Test 4: pagination, sort(/posts)');
+    });
+});
