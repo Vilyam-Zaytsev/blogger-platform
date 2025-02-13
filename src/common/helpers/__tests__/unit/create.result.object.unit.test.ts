8a8e2cc344ef481f4edbf951f39dae15df2ba6cf
Vilyam <z.v.m.365@gmail.com>
2025-02-13 23:31:36 +0300

    test: add tests for ResultObject

--- src/common/helpers/__tests__/unit/create.result.object.unit.test.ts
+++ src/common/helpers/__tests__/unit/create.result.object.unit.test.ts
@@ -4,7 +4,7 @@ import {ResultStatus} from "../../../types/result-types/result-status";
 
 describe('CREATE RESULT OBJECT', () => {
 
-    it('should create a result object with the Successful status, with the "data: null" field (using the static' +
+    it('should create a result object with the Success status, with the "data: null" field (using the static' +
         ' "positive" method of the Result Object class).', () => {
 
             const resultObject: ResultObject<null> = ResultObject
@@ -21,7 +21,7 @@ describe('CREATE RESULT OBJECT', () => {
             console_log_unit(resultObject, 'Test 1: Create result object');
     });
 
-    it('should create a result object with the Successful status, with data (using the static "positive" method of the Result Object class).', () => {
+    it('should create a result object with the Success status, with data (using the static "positive" method of the Result Object class).', () => {
 
             const resultObject: ResultObject<string> = ResultObject
                 .positive(
@@ -38,7 +38,7 @@ describe('CREATE RESULT OBJECT', () => {
             console_log_unit(resultObject, 'Test 2: Create result object');
     });
 
-    it('should create a result object with the Successful status that does not contain data (using the SuccessResult class).', () => {
+    it('should create a result object with the Success status that does not contain data (using the SuccessResult class).', () => {
 
         const resultObject = SuccessResult
             .create(null);
@@ -50,5 +50,19 @@ describe('CREATE RESULT OBJECT', () => {
         });
 
         console_log_unit(resultObject, 'Test 3: Create result object');
+    });
+
+    it('should create a result object with the Success status that contains the data (using the SuccessResult class).', () => {
+
+        const resultObject = SuccessResult
+            .create<string>('success');
+
+        expect(resultObject).toMatchObject({
+            status: ResultStatus.Success,
+            extensions: [],
+            data: 'success'
+        });
+
+        console_log_unit(resultObject, 'Test 4: Create result object');
     });
 });
