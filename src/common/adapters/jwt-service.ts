95e9d4ae299218b0599f6f2ab65b7290b05f1e63
Vilyam <z.v.m.365@gmail.com>
2025-02-15 22:07:12 +0300

    fix

--- src/common/adapters/jwt-service.ts
+++ src/common/adapters/jwt-service.ts
@@ -4,26 +4,22 @@ import {LoginSuccessViewModel} from "../../01-auth/types/login-success-view-mode
 
 const jwtService = {
 
-    async createAccessToken(userId: string, ): Promise<string> {
+    async createAccessToken(userId: string,): Promise<string> {
 
-        const accessToken: string = jwt.sign(
+        return jwt.sign(
             {userId},
             SETTINGS.JWT_SECRET_AT,
             {expiresIn: SETTINGS.JWT_EXPIRATION_RT}
         );
-
-        return accessToken;
     },
 
-    async createRefreshToken(userId: string, ): Promise<string> {
+    async createRefreshToken(userId: string,): Promise<string> {
 
-        const refreshToken: string = jwt.sign(
+        return jwt.sign(
             {userId},
             SETTINGS.JWT_SECRET_RT,
             {expiresIn: SETTINGS.JWT_EXPIRATION_RT}
         );
-
-        return refreshToken;
     },
 
     async verifyToken(token: string): Promise<{ userId: string } | null> {
