8a6c55ae0b2a49ebded7218031c7cecc8408b1bf
Vilyam <z.v.m.365@gmail.com>
2025-03-25 22:31:31 +0300

    refactor(apiTraffic): refactor apiTraffic entity to use Mongoose model

--- src/common/middlewares/rate-limits-guard.ts
+++ src/common/middlewares/rate-limits-guard.ts
@@ -1,6 +1,6 @@
 import {NextFunction, Request, Response} from "express";
 import {SETTINGS} from "../settings";
-import {ApiTrafficModel} from "../../archive/models/api-traffic-model";
+import {ApiTrafficModel} from "../../04-users/domain/api-traffic-entity";
 
 const rateLimitsGuard = async (
     req: Request,
